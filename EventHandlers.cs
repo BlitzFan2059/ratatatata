bb6e78a03448588989d6a0f6f4880a685aede3ab
Appearedzoo <92407653+Appearedzoo@users.noreply.github.com>
2025-04-28 21:41:30 -0400

    Explodes with passion

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,53 +1,41 @@
-﻿using Exiled.API.Enums;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
 using Exiled.Events.EventArgs;
 using Exiled.Events.EventArgs.Player;
 using PlayerRoles;
 using UnityEngine;
-using Exiled.API.Features.Pickups.Projectiles;
+
 namespace FlashbangGun
 {
-            public Throwable ThrowGrenade(ProjectileType type, bool fullForce = true)
-        {
-            Throwable throwable = type switch
-            {
-                ProjectileType.Flashbang => new FlashGrenade(),
-                ProjectileType.Scp2176 => new Scp2176(),
-                _ => new ExplosiveGrenade(type.GetItemType()),
-            };
-
-            ThrowItem(throwable, fullForce);
-            return throwable;
-        }
     public class EventHandlers
     {
-                public int d = 1;
+        public int d = 1;
         private readonly FlashbangGun _plugin;
 
         public EventHandlers(FlashbangGun plugin) => _plugin = plugin;
 
         public void OnShooting(ShootingEventArgs ev)
         {
-                    
-                                d++;
-            // you can change weapon type from there
             if (ev.Player.CurrentItem?.Type == ItemType.GunCom45)
             {
                 ev.IsAllowed = false;
 
-                // you can change to frag grenade if you want
-                
-                 
-                    throwable throwable = (throwable)Item.Create(ItemType.SCP018);
+                {
+                    d++;
+                    Throwable throwable = (Throwable)Item.Create(ItemType.SCP018);
                     ev.Player.ThrowItem(throwable, true);
-                // you foon linging
-                        
+                    ev.Player.Health -= 10;
+                    if (d > 10)
+                    { ev.Player.Explode();
+                        d = 0;
+                    }
+                }
+
                 if (_plugin.Config.Debug)
                 {
                     Log.Info($"{ev.Player.Nickname} this guy fired the flashbang gun.");
                 }
-                        if (d == 10) {ev.Player.Explode()}
             }
         }
     }
